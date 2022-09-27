2cb1d260cfb59a4614fc219e400928ce0b65d86f
fatahprakoso <67370171+fatahprakoso@users.noreply.github.com>
2022-09-27 20:32:27 +0700

    initial commit

---
+++ src/navigation/vertical/pages.jsx
@@ -0,0 +1,29 @@
+import { Award } from 'iconsax-react';
+
+import IntlMessages from "../../layout/components/lang/IntlMessages";
+
+const pages = [
+    {
+        header: <IntlMessages id="sidebar-pages" />,
+    },
+    {
+        id: "blank-page",
+        title: <IntlMessages id="sidebar-pages-blank-page" />,
+        icon: <Award size={18} />,
+        navLink: "/pages/blank-page",
+    },
+    {
+        id: "errors",
+        title: <IntlMessages id="sidebar-pages-error" />,
+        icon: <Award size={18} />,
+        children: [
+            {
+                id: "error-404",
+                title: "404",
+                navLink: "/pages/error-404",
+            },
+        ],
+    },
+];
+
+export default pages
---
+++ src/router/routes/Pages.jsx
@@ -0,0 +1,17 @@
+import { lazy } from "react";
+
+const PagesRoutes = [
+  // PAGES
+  {
+    path: "/pages/blank-page",
+    component: lazy(() => import("../../view/pages/blank")),
+    layout: "VerticalLayout",
+  },
+  {
+    path: "/pages/error-404",
+    component: lazy(() => import("../../view/pages/errors/404")),
+    layout: "FullLayout",
+  },
+];
+
+export default PagesRoutes;
