3084a6098fc4b6dd8eeda87d815ba7e816f3eb07
fatahprakoso <67370171+fatahprakoso@users.noreply.github.com>
2022-10-01 00:11:04 +0700

    update: navbar structure

--- src/navigation/vertical/index.jsx
+++ src/navigation/vertical/index.jsx
@@ -1,10 +1,11 @@
-import pages from "./pages";
+// import pages from "./pages";
 import menu from "./menu";
 
 /**
  * TODO
  * filtering by user type
  */
-const navigation = [...menu, ...pages];
+// const navigation = [...menu, ...pages];
+const navigation = [...menu];
 
 export default navigation;
