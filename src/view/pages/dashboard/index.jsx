cfb632330a6a639435da88c92f8dcd0d35e9e80a
fatahprakoso <67370171+fatahprakoso@users.noreply.github.com>
2022-10-20 20:10:17 +0700

    redesign dashboard

--- src/view/pages/dashboard/index.jsx
+++ src/view/pages/dashboard/index.jsx
@@ -1,44 +1,137 @@
 import React from "react";
-import axios from "axios";
 
-import { Col, Row } from "antd";
-import Cards from "./Cards";
+import { Col, Row, Card, Statistic, Button, List, Avatar } from "antd";
+import {
+  UserAddOutlined,
+  SettingOutlined,
+  GiftTwoTone,
+} from "@ant-design/icons";
+import {
+  UserOctagon,
+  SecurityUser,
+  Profile2User,
+  ShoppingCart,
+} from "iconsax-react";
 import { dummy } from "./dummy.js";
 
-export default function DashBoard() {
-  React.useEffect(() => {
-    axios({
-      method: "GET",
-      url: "http://127.0.0.1:8000/api/wedding-organizers",
-      headers: {
-        Authorization: "Bearer 12|3LPTRjCLtv4QapwNzdUJPRz9vJPNDic0Qp5HMp48",
-      },
-    }).then((d) => console.log(d));
-  }, []);
-  return (
-    <Row gutter={[32, 32]}>
-      <Col span={24}>
-        <Row>
-          <Col span={24}>
-            <Row gutter={[24, 24]} className="card-container">
-              {dummy?.map((data) => (
-                <Cards data={data} />
-              ))}
-            </Row>
-          </Col>
-        </Row>
-      </Col>
-    </Row>
-  );
-}
+import "./style.css";
 
-{
-  /* <Col span={24}>
-  <Card bordered={false} size='small' className="card">
-    <div className="greet">
-      <Sun1 className="sun-icon" />
-      <p>Selamat Pagi, Admin</p>
-    </div>
-  </Card>
-</Col> */
+export default function DashBoard() {
+  return (
+    <>
+      <Row gutter={[32, 32]}>
+        <Col span={7}>
+          <Card
+            style={{
+              height: "100%",
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+            actions={[
+              <UserAddOutlined key="edit" />,
+              <SettingOutlined key="setting" />,
+            ]}
+          >
+            <Statistic
+              title="Total User"
+              value={10827}
+              prefix={<UserOctagon color="#f47373" size={30} />}
+              // prefixCls="custom-dashboard-statistic"
+            />
+          </Card>
+        </Col>
+        <Col span={10}>
+          <Row>
+            <Col span={24}>
+              <Card>
+                <Statistic
+                  title="Wedding Organizer"
+                  value={830}
+                  prefix={<SecurityUser color="#F45000" size={30} />}
+                  suffix="/ 10,827"
+                />
+              </Card>
+
+              <Card>
+                <Statistic
+                  title="Pelanggan"
+                  value={9997}
+                  prefix={<Profile2User color="#F45000" size={30} />}
+                  suffix="/ 10,827"
+                />
+              </Card>
+            </Col>
+          </Row>
+        </Col>
+        <Col span={7}>
+          <Card
+            style={{
+              height: "100%",
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+            actions={[<Button>Kelola Products</Button>]}
+          >
+            <Statistic
+              title="Pesanan"
+              value={215402}
+              prefix={<ShoppingCart color="#37d67a" />}
+            />
+          </Card>
+        </Col>
+
+        <Col span={12}>
+          <Card>
+            <List
+              header="Best Seller"
+              itemLayout="horizontal"
+              dataSource={[
+                { title: "product 1", description: "lorem ipsum dolor" },
+                { title: "product 2", description: "lorem ipsum dolor" },
+                { title: "product 3", description: "lorem ipsum dolor" },
+              ]}
+              renderItem={(item) => (
+                <List.Item>
+                  <List.Item.Meta
+                    avatar={<GiftTwoTone />}
+                    title={<a href="https://ant.design">{item.title}</a>}
+                    description={item.description}
+                  />
+                </List.Item>
+              )}
+            />
+          </Card>
+        </Col>
+        <Col span={12}>
+          <Card>
+            <List
+              header="Pesanan Terbaru"
+              itemLayout="horizontal"
+              dataSource={[
+                { title: "si A", description: "wo 1" },
+                { title: "si B", description: "wo 2" },
+                { title: "si C", description: "wo 3" },
+              ]}
+              renderItem={(item) => (
+                <List.Item>
+                  <List.Item.Meta
+                    avatar={<Avatar src="https://joeschmoe.io/api/v1/random" />}
+                    title={<a href="https://ant.design">{item.title}</a>}
+                    description={item.description}
+                  />
+                  ini adalah deskripsi dari produk. ini adalah deskripsi dari
+                  produk. ini adalah deskripsi dari produk. ini adalah deskripsi
+                  dari produk.
+                </List.Item>
+              )}
+            />
+          </Card>
+        </Col>
+      </Row>
+    </>
+  );
 }
