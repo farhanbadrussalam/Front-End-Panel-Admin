23dce48f368c131f6f78a0f21db9c70ba3b39345
fatahprakoso <67370171+fatahprakoso@users.noreply.github.com>
2022-10-20 13:58:14 +0700

    redesign produk-ucapan

--- src/router/routes/Pages.jsx
+++ src/router/routes/Pages.jsx
@@ -205,30 +205,23 @@ const PagesRoutes = [
     layout: "VerticalLayout",
   },
   {
-    path: "/admin/produk-ucapan-user/:userid",
-    component: lazy(() =>
-      import("../../view/pages/produk-wo/produk-ucapan-user/produk")
-    ),
-    layout: "VerticalLayout",
-  },
-  {
     path: "/admin/produk-ucapan-user/detail/:userid",
     component: lazy(() =>
-      import("../../view/pages/produk-wo/produk-ucapan-user/produk")
+      import("../../view/pages/produk-wo/produk-ucapan-user/form/read")
     ),
     layout: "VerticalLayout",
   },
   {
     path: "/admin/produk-ucapan-user/edit/:userid",
     component: lazy(() =>
-      import("../../view/pages/produk-wo/produk-ucapan-user/produk")
+      import("../../view/pages/produk-wo/produk-ucapan-user")
     ),
     layout: "VerticalLayout",
   },
   {
     path: "/admin/produk-ucapan-user/delete/:userid",
     component: lazy(() =>
-      import("../../view/pages/produk-wo/produk-ucapan-user/produk")
+      import("../../view/pages/produk-wo/produk-ucapan-user")
     ),
     layout: "VerticalLayout",
   },
