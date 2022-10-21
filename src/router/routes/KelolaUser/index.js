7d7e5a03e1c86c29dc06defc6caef1865ef9014a
fatahprakoso <67370171+fatahprakoso@users.noreply.github.com>
2022-10-21 12:14:49 +0700

    rapihin routes

---
+++ src/router/routes/KelolaUser/index.js
@@ -0,0 +1,67 @@
+import { lazy } from "react";
+
+const kelolaUser = [
+  // get all data
+  {
+    path: "/admin/crud-user/",
+    component: lazy(() => import("../../../view/pages/kelola-user/crud-user")),
+    layout: "VerticalLayout",
+  },
+  {
+    path: "/admin/atur-role",
+    component: lazy(() =>
+      import("../../../view/pages/kelola-user/atur-role-staff")
+    ),
+    layout: "VerticalLayout",
+  },
+  {
+    path: "/admin/policy-user",
+    component: lazy(() =>
+      import("../../../view/pages/kelola-user/atur-permission")
+    ),
+    layout: "VerticalLayout",
+  },
+
+  // get detail data
+  {
+    path: "/admin/crud-user/detail/:userid",
+    component: lazy(() =>
+      import("../../../view/pages/kelola-user/crud-user/form/read")
+    ),
+    layout: "VerticalLayout",
+  },
+  {
+    path: "/admin/policy-user/edit/:roleid",
+    component: lazy(() =>
+      import("../../../view/pages/kelola-user/atur-permission/form/update")
+    ),
+    layout: "VerticalLayout",
+  },
+
+  // create data
+  {
+    path: "/admin/crud-user/create",
+    component: lazy(() =>
+      import("../../../view/pages/kelola-user/crud-user/form/create")
+    ),
+    layout: "VerticalLayout",
+  },
+  {
+    path: "/admin/policy-user/create",
+    component: lazy(() =>
+      import("../../../view/pages/kelola-user/atur-permission/form/create")
+    ),
+    layout: "VerticalLayout",
+  },
+
+  // update data
+  {
+    path: "/admin/crud-user/edit/:userid",
+    component: lazy(() =>
+      import("../../../view/pages/kelola-user/crud-user/form/update")
+    ),
+    layout: "VerticalLayout",
+  },
+];
+
+export default kelolaUser;
