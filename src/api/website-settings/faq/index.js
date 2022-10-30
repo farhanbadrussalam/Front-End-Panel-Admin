a18a9c196abcf7f004c239abec4c528bdbecf676
fatahprakoso <67370171+fatahprakoso@users.noreply.github.com>
2022-10-30 13:13:02 +0700

    add: getter data

---
+++ src/api/website-settings/faq/index.js
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+import { api } from "../../../configs/apiConfig";
+
+export const contacts = () => {
+  const [data, setData] = useState([{}]);
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(true);
+  const [changed, setChanged] = useState(false);
+
+  const resetControlFlowState = () => {
+    setError("");
+    setLoading(true);
+  };
+
+  useEffect(() => {
+    resetControlFlowState();
+
+    api
+      .get("frequently-ask-questions")
+      .then((d) => setData(d.data.data))
+      .catch((e) => setError(e))
+      .finally(() => setLoading(false));
+  }, [changed]);
+
+  return { data, error, loading };
+};
