52d2154a4b823025cdadcc408bba20ee8f5adcbe
fatahprakoso <67370171+fatahprakoso@users.noreply.github.com>
2022-10-30 22:56:53 +0700

    update

--- src/configs/apiConfig.jsx
+++ src/configs/apiConfig.jsx
@@ -1,17 +1,13 @@
 import axios from "axios";
 
+const { NODE_ENV, REACT_APP_API_PROD, REACT_APP_API_DEV } = process.env;
+
 const token = localStorage.getItem("token");
 
-const configDevelopment = {
-  baseURL: "http://127.0.0.1:8000/api/",
-  timeout: 2000,
-  headers: { Authorization: token },
-};
-
-const configProduction = {
-  baseURL: "domain",
+const config = {
+  baseURL: NODE_ENV === "production" ? REACT_APP_API_PROD : REACT_APP_API_DEV,
   timeout: 1000,
   headers: { Authorization: token },
 };
 
-export const api = axios.create(configDevelopment);
+export const api = axios.create(config);
