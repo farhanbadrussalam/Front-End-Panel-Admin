194287f1de5d83c91a5cd14d5d384e7b391e07d9
fatahprakoso <67370171+fatahprakoso@users.noreply.github.com>
2022-11-03 14:25:12 +0700

    add: page entry point

--- src/view/pages/websiteSettings/index.jsx
+++ src/view/pages/websiteSettings/index.jsx
@@ -1,7 +1,56 @@
-export default function () {
+import { useEffect, useState } from "react";
+
+import { List, Skeleton, Avatar } from "antd";
+
+const ContactList = ({ data }) => {
   return (
     <>
-      <h1>Hello World</h1>
+      <List
+        itemLayout="horizontal"
+        dataSource={data}
+        renderItem={(item) => (
+          <>
+            <List.Item
+              actions={[
+                <a key="list-loadmore-edit">edit</a>,
+                <a key="list-loadmore-more">more</a>,
+              ]}
+            >
+              <Skeleton avatar title={false} loading={item.loading} active>
+                <List.Item.Meta title={item.name} description={item.value} />
+                <div>{item.status}</div>
+              </Skeleton>
+            </List.Item>
+          </>
+        )}
+      />
+    </>
+  );
+};
+
+export default function () {
+  const [contacts, setContacts] = useState([
+    {
+      id: 1,
+      name: "Line",
+      value: "@wo-best-wishes-1",
+      status: 1,
+    },
+    {
+      id: 2,
+      name: "Line",
+      value: "@wo-best-wishes-1",
+      status: 1,
+    },
+  ]);
+
+  useEffect(() => {
+    // set dummy contacts using api
+  }, []);
+
+  return (
+    <>
+      <ContactList data={contacts} />
     </>
   );
 }
