730a3ef2cb5153e84d7c904c1277ddfa8655b3d2
fatahprakoso <67370171+fatahprakoso@users.noreply.github.com>
2022-11-05 13:39:07 +0700

    add: admin contact list

--- src/view/pages/websiteSettings/index.jsx
+++ src/view/pages/websiteSettings/index.jsx
@@ -1,34 +1,11 @@
 import { useEffect, useState } from "react";
 
-import { List, Skeleton, Avatar } from "antd";
+import { List, Skeleton, Button, Form, Input, Space } from "antd";
+import { Trash } from "iconsax-react";
 
-const ContactList = ({ data }) => {
-  return (
-    <>
-      <List
-        itemLayout="horizontal"
-        dataSource={data}
-        renderItem={(item) => (
-          <>
-            <List.Item
-              actions={[
-                <a key="list-loadmore-edit">edit</a>,
-                <a key="list-loadmore-more">more</a>,
-              ]}
-            >
-              <Skeleton avatar title={false} loading={item.loading} active>
-                <List.Item.Meta title={item.name} description={item.value} />
-                <div>{item.status}</div>
-              </Skeleton>
-            </List.Item>
-          </>
-        )}
-      />
-    </>
-  );
-};
-
-export default function () {
+const ContactList = () => {
+  // const [isAdding, setIsAdding] = useState(false);
+  const [isAdding, setIsAdding] = useState(true);
   const [contacts, setContacts] = useState([
     {
       id: 1,
@@ -43,14 +20,140 @@ export default function () {
       status: 1,
     },
   ]);
+  const [name, setName] = useState("");
+  const [value, setValue] = useState("");
 
-  useEffect(() => {
-    // set dummy contacts using api
-  }, []);
+  const statusConverter = (status) => {
+    switch (status) {
+      case 1:
+        return <div style={{ color: "green" }}>aktif</div>;
+      default:
+        return <div style={{ color: "red" }}>status not found!</div>;
+    }
+  };
+
+  const submitHandler = (e) => {
+    name && value && setIsAdding(false);
+  };
 
   return (
     <>
-      <ContactList data={contacts} />
+      <div className="custom_website-settings_contacts">
+        <List
+          itemLayout="horizontal"
+          dataSource={contacts}
+          header={<div>Kontak Admin</div>}
+          renderItem={(item) => (
+            <>
+              <List.Item
+                actions={[
+                  <div key="list-loadmore-edit">
+                    <Trash color="red" size={20} />
+                  </div>,
+                ]}
+              >
+                <Skeleton avatar title={false} loading={item.loading} active>
+                  <List.Item.Meta title={item.name} description={item.value} />
+                  <div>{statusConverter(item.status)}</div>
+                </Skeleton>
+              </List.Item>
+            </>
+          )}
+        />
+        {isAdding ? (
+          <div className="addForm">
+            <Form
+              name="addForm"
+              labelCol={{
+                span: 8,
+              }}
+              wrapperCol={{
+                span: 8,
+              }}
+              autoComplete="off"
+            >
+              <Form.Item
+                label="Nama Kontak"
+                name="name"
+                rules={[
+                  {
+                    required: true,
+                    message: "Mohon masukkan nama kontak!",
+                  },
+                ]}
+              >
+                <Input
+                  value={name}
+                  placeholder="Whatsapp, Line, Telegram"
+                  onChange={(e) => setName(e.target.value)}
+                />
+              </Form.Item>
+              <Form.Item
+                label="Kontak"
+                name="value"
+                rules={[
+                  {
+                    required: true,
+                    message: "Mohon masukkan kontak!",
+                  },
+                ]}
+              >
+                <Input
+                  value={value}
+                  placeholder="@admin123, 089..."
+                  onChange={(e) => setValue(e.target.value)}
+                />
+              </Form.Item>
+              <Form.Item
+                wrapperCol={{
+                  offset: 8,
+                  span: 8,
+                }}
+              >
+                <Space
+                  size="small"
+                  style={{ display: "flex", justifyContent: "space-evenly" }}
+                >
+                  <Button
+                    htmlType="submit"
+                    type="primary"
+                    danger
+                    onClick={submitHandler}
+                  >
+                    Simpan
+                  </Button>
+                  <Button
+                    danger
+                    htmlType="button"
+                    onClick={() => setIsAdding(false)}
+                  >
+                    Batal
+                  </Button>
+                </Space>
+              </Form.Item>
+            </Form>
+          </div>
+        ) : (
+          <div className="button">
+            <Button
+              size="small"
+              type="primary"
+              danger
+              onClick={() => setIsAdding(true)}
+            >
+              tambah kontak admin
+            </Button>
+          </div>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default function () {
+  return (
+    <>
+      <ContactList />
     </>
   );
 }
