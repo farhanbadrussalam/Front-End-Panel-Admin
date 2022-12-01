dd56978589b4145c8432557299eee35bb485aebf
KevinDanudoro <kevindanudoro68@gmail.com>
2022-12-01 23:32:01 +0700

    fix: revisi tampilan dan atur crud button pada form

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -119,8 +119,8 @@ export default function Router() {
                   );
                 } else {
                   if (auth) {
-                    if (permission.includes(route.path) || route.path == "/admin/dashboard") {
-                      // if (true) {
+                    // if (permission.includes(route.path) || route.path == "/admin/dashboard") {
+                    if (true) {
                       if (type == 2 && route.type == type || type == 1) {
                         return (
                           <Route
--- src/view/pages/kelola-user/crud-user/CrudUser.jsx
+++ src/view/pages/kelola-user/crud-user/CrudUser.jsx
@@ -4,12 +4,14 @@ import { getUsers } from '../../../../api/kelola-user/getUser'
 import TableDisplay from '../../../components/custom-components/TableDisplay'
 import TableCard from '../../../components/custom-components/TableCard'
 import Column from './UserColumn'
+import { usePermissionContext } from '../../../../context/PermissionContext'
 
 const index = () => {
   let { data, deleteUser } = getUsers()
+  const { permission } = usePermissionContext()
 
   data = data?.map((values) => (
-    { ...values, deleteUser: deleteUser }
+    { ...values, deleteUser: deleteUser, permission }
   ))
 
   return (
